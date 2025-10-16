4e0f557c6fceaa5c7095c1912453ac8d981c5807
soph <sophearle16@gmail.com>
2025-10-16 13:29:51 +0100

    did guide 4a

---
+++ Soph AGP Guide 4a/VertexShader.hlsl
@@ -0,0 +1,19 @@
+struct VIn
+{
+    float3 position : POSITION;
+    float4 colour : COLOUR;
+};
+
+struct VOut
+{
+    float4 position : SV_Position;
+    float4 colour : COLOUR;
+};
+
+VOut main( VIn input )
+{
+    VOut output;
+    output.position = float4(input.position, 1);
+    output.colour = input.colour;
+	return output;
+}
